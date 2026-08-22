20e4e21f5965acfb412542b4727f5680e9a013cc
chiraitori <chiraitori@users.noreply.github.com>
2026-08-23 03:04:51 +0700

    feat: set live HTTPS API domain https://api-manga.chiraitori.dev/api/v1 (v1.0.2)

--- src/MangaHost/MangaHost.ts
+++ src/MangaHost/MangaHost.ts
@@ -15,14 +15,14 @@ import {
 import { MangaHostParser } from './MangaHostParser'
 
 export const MangaHostInfo: SourceInfo = {
-  version: '1.0.1',
+  version: '1.0.2',
   name: 'MangaHost R2',
   icon: 'icon.png',
   author: 'chiraitori',
   authorWebsite: 'https://github.com/chiraitori',
   description: 'Extension đọc manga từ Cloudflare R2 MangaHost API',
   contentRating: ContentRating.EVERYONE,
-  websiteBaseURL: 'https://api.manga.chiraitori.dev/api/v1'
+  websiteBaseURL: 'https://api-manga.chiraitori.dev/api/v1'
 }
 
 export class MangaHost extends Source {
